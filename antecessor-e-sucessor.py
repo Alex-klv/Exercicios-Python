170b15eac784b914eba5376b1558850cba2f5ad8
Alex <135345246+Alex-klv@users.noreply.github.com>
2024-11-12 11:00:27 -0300

    Programa de antecessor e sucessor de numero
    Um programa básico em Python para encontrar o antecessor e o sucessor de um número à sua escolha.

---
+++ antecessor-e-sucessor.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero para saber seu antecessor e sucessor: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("O numero {} o seu antecessor é {} e o seu sucessor {}".format (numero, antecessor, sucessor))
