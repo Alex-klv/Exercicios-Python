d44775ca198622bf51745280dadc96484b0c10ea
Alex <135345246+Alex-klv@users.noreply.github.com>
2025-01-15 08:41:58 -0300

    calculadora-multiplicação
    Este projeto é uma calculadora simples, desenvolvida em Python, que gera a tabuada de multiplicação de números inteiros de 1 a 10.

---
+++ calculadora-multiplicação.py
@@ -0,0 +1,25 @@
+valor = int(input("Digite um número para ver a sua tabuada: "))
+
+x1 = valor * 1
+x2 = valor * 2
+x3 = valor * 3
+x4 = valor * 4
+x5 = valor * 5
+x6 = valor * 6
+x7 = valor * 7
+x8 = valor * 8
+x9 = valor * 9
+x10 = valor * 10
+
+print("----------------")
+print("{} x 1 = {}".format(valor, x1))
+print("{} x 2 = {}".format(valor, x2))
+print("{} x 3 = {}".format(valor, x3))
+print("{} x 4 = {}".format(valor, x4))
+print("{} x 5 = {}".format(valor, x5))
+print("{} x 6 = {}".format(valor, x6))
+print("{} x 7 = {}".format(valor, x7))
+print("{} x 8 = {}".format(valor, x8))
+print("{} x 9 = {}".format(valor, x9))
+print("{} x 10 = {}".format(valor, x10))
+print("----------------")
