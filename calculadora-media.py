1417ec5f4d7eb5b884565fbcd7316cf6e7bc063f
Alex <135345246+Alex-klv@users.noreply.github.com>
2025-01-15 08:40:32 -0300

    calculadora-media
    Calculadora para calcular a média entre duas notas fornecidas pelo usuário.

---
+++ calculadora-media.py
@@ -0,0 +1,8 @@
+print("Bem vindo a calculadora de media! \n")
+
+nota1 = float(input("Digite a primeira nota do aluno: "))
+nota2 = float(input("Digite a segunda nota do aluno: "))
+
+media = (nota1 + nota2) / 2
+
+print("Sua media é {}".format(media))
