55d302149bace77d95db2c3b009fc572157aa7e8
Alex <135345246+Alex-klv@users.noreply.github.com>
2025-01-15 08:43:15 -0300

    calculadora-reais-p-dolar
    Um programa básico para conversão de Dólar para Reais desenvolvido em Python.

---
+++ calculadora-reais-p-dolar.py
@@ -0,0 +1,7 @@
+print("Bem-vindo a calculadora de reais para dolar!")
+
+conversor = float(input("Quanto dinheiro você tem na carteira? R$"))
+
+resultado = conversor * 6.15
+
+print("Com {} você pode comprar US${}".format(conversor, resultado))
