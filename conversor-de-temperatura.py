fa07baf870536614b534eea56633db424a33c85a
Alex <135345246+Alex-klv@users.noreply.github.com>
2025-01-15 08:46:10 -0300

    conversor-de-temperatura
    Um programa básico em Python com o objetivo de converter temperaturas de graus Celsius para Fahrenheit.

---
+++ conversor-de-temperatura.py
@@ -0,0 +1,7 @@
+print("Bem-vindo ao conversor de temperatura!\n")
+
+C = float(input("Informe a sua temperatura em °C: "))
+
+F = (C * 1.8) + 32
+
+print("A temperatura de {}°C em Fahrenheit corresponde a {}°F".format(C, F))
