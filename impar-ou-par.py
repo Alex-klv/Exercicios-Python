5581aac6af6cdfc2b2f6167679e0a0f075cbac4b
Alex <135345246+Alex-klv@users.noreply.github.com>
2025-01-16 23:32:33 -0300

    impar-ou-par
    Este é um programa em Python que determina se um número é par ou ímpar.

---
+++ impar-ou-par.py
@@ -0,0 +1,9 @@
+print("Bem-vindo ao programa de impar ou par!\n")
+
+numero = int(input("Digite um numero: "))
+
+if numero % 2 == 0:
+    print("Seu número é par!")
+
+else:
+    print("Seu número é impar!")
