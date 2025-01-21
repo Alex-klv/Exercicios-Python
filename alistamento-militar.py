ebe91cb7178ad2b1306ddd23831d9b9cea2188e1
Alex <135345246+Alex-klv@users.noreply.github.com>
2025-01-21 12:07:35 -0300

    alistamento-militar
    Este programa em Python determina, com base no seu ano de nascimento, se você ainda vai se alistar, se já está no período de alistamento militar ou se já passou da idade para se alistar.

---
+++ alistamento-militar.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+print("Bem-vindo ao programa do alistamento militar!\n")
+
+nascimento = int(input("Digite seu ano de nascimento: "))
+
+anoatual = date.today().year
+idade = anoatual - nascimento
+
+if idade > 18:
+    passado = idade - 18
+    anoatras = anoatual - passado
+    print("Você nasceu em {} tem {} anos em {}.".format(nascimento, idade, anoatual))
+    print("Você já deveria ter se alistado há {} anos.".format(passado))
+    print("Seu alistamento foi em {}.".format(anoatras))
+elif idade < 18:
+    futuro = 18 - idade
+    anofuturo = anoatual - futuro
+    print("Você nasceu em {} tem {} anos em {}.".format(nascimento, idade, anoatual))
+    print("Você deverá se alistar daqui há {} anos.".format(futuro))
+    print("Seu alistamento será em {}.".format(anofuturo))
+else:
+    print("Você nasceu em {} tem {} anos em {}.".format(nascimento, idade, anoatual))
+    print("Você deve se alistar IMEDIATAMENTE!")
